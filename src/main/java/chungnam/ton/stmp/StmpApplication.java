ab992c4a68eef2db4a701ade62bbfd674ace5b79
T-ferret <hs381717@gmail.com>
2025-07-14 21:39:21 +0900

    기본 세팅

---
+++ src/main/java/chungnam/ton/stmp/StmpApplication.java
@@ -0,0 +1,13 @@
+package chungnam.ton.stmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StmpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chungnam/ton/stmp/StmpApplicationTests.java
@@ -0,0 +1,13 @@
+package chungnam.ton.stmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StmpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
